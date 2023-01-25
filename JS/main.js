eef37f0102a0b17fc54b34c63b72037c563a21e2
Epic_Alberto <edyal2222@gmail.com>
2023-01-25 03:31:52 -0600

    la funcionalidad está casi terminada me sigo atorando en la parte del diseño del epp

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,75 @@
+import relacion from './trabajo_epp.json' assert { type : "json"};
+
+function textoAOracion(oracion) {
+    let palabras = oracion.split(" ").map(palabra => {
+        return palabra[0].toUpperCase() + palabra.slice(1);
+    })
+    return palabras.join(" ");
+}
+
 $(document).ready(function() {
 
-    $(".contenedor").on("click", function() {
-        $(".fase:visible").fadeOut("slow", function() {
-            if ($(this).hasClass("tiempo")) {
-                $(".fase").first().fadeIn();
+    // $(".contenedor").on("click", function() {
+    //     $(".fase:visible").fadeOut("slow", function() {
+    //         if ($(this).hasClass("tiempo")) {
+    //             $(".fase").first().fadeIn();
+    //         } else {
+    //             $(this).next().fadeIn();
+    //         }
+    //     });
+    // });
+
+    $(".contenedor").fadeIn(800, function() {
+        $(".nombre .entrada").focus().keydown(function(e) {
+            if (e.which == 13) {
+                e.preventDefault();
+                $(".siguiente").click();
+            }
+        }).keyup(function() {
+            if ($(this).text().trim() != "") {
+                $(".botones").animate({opacity:1});
             } else {
-                $(this).next().fadeIn();
+                $(".botones").animate({opacity:0});
             }
         });
     });
+
+    $(".borrar").click(function() {
+        $(".nombre .entrada").text("");
+        $(".botones").animate({opacity:0});
+    });
+    
+    $(".siguiente").click(function() {
+        var nombre = textoAOracion($(".nombre .entrada").text().trim());
+        $(".nombre").fadeOut("slow", function(){
+            $(".area").fadeIn("slow");
+            console.log(nombre);
+        })
+    });
+
+    $(".areas .opcion").click(function() {
+        var area = $(this).text();
+        $(".area").fadeOut("slow", function(){
+            $(".trabajo").fadeIn("slow");
+        })
+        console.log(area);
+    });
+
+    $(".trabajos .opcion").click(function() {
+        var trabajo = $(this).text();
+        console.log(trabajo);
+        $(".trabajo").fadeOut("slow", function(){
+            relacion.trabajos.forEach(elemento => {
+                if (elemento.nombre == trabajo) {
+                    console.log(elemento.epp);
+                    elemento.epp.forEach(epp => {
+                        $(".caja:has(.imagen[alt="+epp+"])").show();
+                    });
+                    return;
+                }
+            });
+            $(".equipo").fadeIn("slow");
+        });
+    });
+
 })
